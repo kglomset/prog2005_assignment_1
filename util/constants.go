e1b86332b5816e66055cecaba69be45c2cfec392
Kristian <kglomset@gmail.com>
2024-03-06 13:45:12 +0100

    Small change to the REST countries endpoint URL.

--- util/constants.go
+++ util/constants.go
@@ -1,7 +1,7 @@
 package util
 
 const GutendexEndPoint = "http://129.241.150.113:8000/books/"
-const RestCountriesEndPoint = "https://restcountries.com/v3.1/alpha/" //May have to edit this endpoint
+const RestCountriesEndPoint = "http://129.241.150.113:8080/v3.1/alpha/" //May have to edit this endpoint
 const L2CEndPoint = "http://129.241.150.113:3000/language2countries/"
 const BookCountEndPoint = "/librarystats/v1/bookcount/"
 const ReadershipEndPoint = "/librarystats/v1/readership/"
